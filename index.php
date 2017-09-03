2848d353527a7424f04efc3adaacdf3b4aed2606
Karan Jit Singh <karan1866@gmail.com>
2017-09-03 14:39:13 +0530

    use relative paths

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!doctype html>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="/style/page.css"/>
-    <script type="text/javascript" src="/scripts/particle.js"></script>
+    <link rel="stylesheet" type="text/css" href="./style/page.css"/>
+    <script type="text/javascript" src="./scripts/particle.js"></script>
 </head>
 <body>
     <div class="main-container">
         <canvas id="canvas"></canvas>
     </div>
 </body>
-<script type="text/javascript" src="/scripts/page.js"></script>
+<script type="text/javascript" src="./scripts/page.js"></script>
 </html>
