aa6760d1b434f24e03e2dd21d9937589e7957b66
Karan Jit Singh <karan1866@gmail.com>
2017-09-11 00:42:47 +0530

    completed page animation added contact page

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <!doctype html>
+<!--
+
+    Hello there fellow coder!
+
+    It seems to me this particle animation thingy has peaked your
+    interest. Won't it be more fun to read the uncompressed and
+    uncompiled TypeScript it was written in?
+    
+    Let me know if I've earned your star on GitHub.
+    https://github.com/karanjitsingh.com/particle.js
+
+-->
 <html>
 <head>
+<!--
+
+    Ahh! I guess you still insist on doing it your way ;)
+    I already like you.
+
+-->
     <link rel="stylesheet" type="text/css" href="./style/page.css"/>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300' rel='stylesheet' type='text/css'>
@@ -12,9 +30,9 @@
     </div>
     <div id="menu-container">
         <div class="menu">
-            <a>Code</a>
-            <a>About</a>
-            <a>Contact</a>
+            <a href="/code/">Code</a>
+            <a href="/about/">About</a>
+            <a href="/contact/">Contact</a>
         </div>
     </div>
     <div id="github-banner">
@@ -24,6 +42,34 @@
             </svg>
         </a>
     </div>
+    <div id="pages-container">
+        <div id="code-page"></div>
+        <div id="about-page"></div>
+        <div id="contact-page">
+            <div>
+                <div>
+                    <span>Leave a message.</span>
+                    <div>
+                        <a href="mailto:karan1866@gmail.com" target="_blank">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 24 24" fill="#fff"><path d="M12 .02c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.99 6.98l-6.99 5.666-6.991-5.666h13.981zm.01 10h-14v-8.505l7 5.673 7-5.672v8.504z"/></svg>
+                        </a>
+                        <a href="https://facebook.com/karan1866/" target="_blank">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 24 24" fill="#fff"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3 8h-1.35c-.538 0-.65.221-.65.778v1.222h2l-.209 2h-1.791v7h-3v-7h-2v-2h2v-2.308c0-1.769.931-2.692 3.029-2.692h1.971v3z"/></svg>
+                        </a>
+                        <a href="http://in.linkedin.com/in/kjitsingh/" target="_blank">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 24 24" fill="#fff"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 16h-2v-6h2v6zm-1-6.891c-.607 0-1.1-.496-1.1-1.109 0-.612.492-1.109 1.1-1.109s1.1.497 1.1 1.109c0 .613-.493 1.109-1.1 1.109zm8 6.891h-1.998v-2.861c0-1.881-2.002-1.722-2.002 0v2.861h-2v-6h2v1.093c.872-1.616 4-1.736 4 1.548v3.359z"/></svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a id="page-close">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" style="transform:rotate(90deg)" fill="#fff">
+                <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"></path>
+            </svg>
+        </a>
+    </div>
 </body>
+<script type="text/javascript" src="./scripts/preload.js"></script>
 <script type="text/javascript" src="./scripts/page.js"></script>
 </html>
---
+++ scripts/preload.js
@@ -0,0 +1,109 @@
+function $id(src) {
+    return document.getElementById(src);
+}
+
+var EasingFunctions = {
+    // no easing, no acceleration
+    linear: function (t) { return t },
+    // accelerating from zero velocity
+    easeInQuad: function (t) { return t*t },
+    // decelerating to zero velocity
+    easeOutQuad: function (t) { return t*(2-t) },
+    // acceleration until halfway, then deceleration
+    easeInOutQuad: function (t) { return t<.5 ? 2*t*t : -1+(4-2*t)*t },
+    // accelerating from zero velocity
+    easeInCubic: function (t) { return t*t*t },
+    // decelerating to zero velocity
+    easeOutCubic: function (t) { return (--t)*t*t+1 },
+    // acceleration until halfway, then deceleration
+    easeInOutCubic: function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 },
+    // accelerating from zero velocity
+    easeInQuart: function (t) { return t*t*t*t },
+    // decelerating to zero velocity
+    easeOutQuart: function (t) { return 1-(--t)*t*t*t },
+    // acceleration until halfway, then deceleration
+    easeInOutQuart: function (t) { return t<.5 ? 8*t*t*t*t : 1-8*(--t)*t*t*t },
+    // accelerating from zero velocity
+    easeInQuint: function (t) { return t*t*t*t*t },
+    // decelerating to zero velocity
+    easeOutQuint: function (t) { return 1+(--t)*t*t*t*t },
+    // acceleration until halfway, then deceleration
+    easeInOutQuint: function (t) { return t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t }
+}
+
+function Animation(step, easing, duration, callback) {
+	this.start = null;
+	this.easing = easing;
+	this.duration = duration;
+	this.callback = callback;
+	this.step = step;
+
+	this.animate = function(time) {
+		if(this.start === null)
+			this.start = time;
+
+		if(time >= this.start + duration) {
+			this.step(1);
+			if(this.callback)
+				this.callback();
+		}
+		else {
+            this.step(this.easing((time - this.start)/this.duration));
+			window.requestAnimationFrame(this.animate.bind(this));
+        }
+	};
+
+	window.requestAnimationFrame(this.animate.bind(this));
+}
+
+function loadingComplete(callback) {
+
+    function hideWave() {
+        new Animation(function (t) {
+            wave.alpha = 1-t
+        }, EasingFunctions.easeOutCubic, 300, callback);
+    }
+
+    new Animation(function (t) {
+        wave.waves[1].amplitude = 40*(1-t);
+        wave.options.top = 2*H/3 - t*(H/6);
+    }, EasingFunctions.easeOutCubic, 1000, callback);
+}
+
+var canvas = $id("canvas");
+var W = document.body.clientWidth;
+var H = document.body.clientHeight;
+var total = (400 / 1400) * W;
+total = total < 450 ? 450 : total;
+var t;
+canvas.width = W;
+canvas.height = H;
+
+var pjs = new ParticleJS(canvas, null,{drawCanvasBackground: true, canvasBGColor: "#2F5168"});
+
+var wave = new ParticleJSAnimations.WaveAnimation(total, [
+	{
+		time: 0,
+		amplitude: 100,
+		wavelength: 20,
+		phase: 0,
+		timePeriod: 10,
+		increment: 0.1
+	},
+	{
+		time: 0,
+		amplitude: 40,
+		wavelength: 1000,
+		phase: 0,
+		timePeriod: 10,
+		increment: 0.1
+	}
+], {atomOptions: {colorSet: ["#E04836", "#F39D41", "#DDDDDD"],  popProbability:1}, top: 2*H/3, width: W});
+
+pjs.addDrawObject(wave);
+pjs.start();
+
+window.addEventListener('load', function() {
+	setTimeout(function() {loadingComplete(initPage);}, 100);
+});
+
