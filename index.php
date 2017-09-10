83292ab66fcd8e49010ccf7f9069e8139ae11df4
Karan Jit Singh <karan1866@gmail.com>
2017-09-10 20:09:44 +0530

    completed main page

--- index.php
+++ index.php
@@ -2,12 +2,26 @@
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="./style/page.css"/>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300' rel='stylesheet' type='text/css'>
     <script type="text/javascript" src="./scripts/particle.js"></script>
 </head>
 <body>
-    <div class="main-container">
+    <div id="canvas-container">
         <canvas id="canvas"></canvas>
     </div>
+    <div id="menu-container">
+        <div class="menu">
+            <a>Code</a>
+            <a>About</a>
+            <a>Contact</a>
+        </div>
+    </div>
+    <a id="github-banner" href="https://github.com/karanjitsingh/particle.js" target="_blank">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path d="M21 3c0-1.657-1.343-3-3-3s-3 1.343-3 3c0 1.323.861 2.433 2.05 2.832.168 4.295-2.021 4.764-4.998 5.391-1.709.36-3.642.775-5.052 2.085v-7.492c1.163-.413 2-1.511 2-2.816 0-1.657-1.343-3-3-3s-3 1.343-3 3c0 1.305.837 2.403 2 2.816v12.367c-1.163.414-2 1.512-2 2.817 0 1.657 1.343 3 3 3s3-1.343 3-3c0-1.295-.824-2.388-1.973-2.808.27-3.922 2.57-4.408 5.437-5.012 3.038-.64 6.774-1.442 6.579-7.377 1.141-.425 1.957-1.514 1.957-2.803zm-16.8 0c0-.993.807-1.8 1.8-1.8s1.8.807 1.8 1.8-.807 1.8-1.8 1.8-1.8-.807-1.8-1.8zm3.6 18c0 .993-.807 1.8-1.8 1.8s-1.8-.807-1.8-1.8.807-1.8 1.8-1.8 1.8.807 1.8 1.8zm10.2-16.2c-.993 0-1.8-.807-1.8-1.8s.807-1.8 1.8-1.8 1.8.807 1.8 1.8-.807 1.8-1.8 1.8z"></path>
+        </svg>
+    </a>
 </body>
 <script type="text/javascript" src="./scripts/page.js"></script>
 </html>
